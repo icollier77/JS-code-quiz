a6f3776ac05856aa0c6fc018775eccfeb7a6a1e7
Inessa <143314129+icollier77@users.noreply.github.com>
2023-12-31 23:28:02 +0000

    refactored code for clarity

--- starter/assets/js/logic.js
+++ starter/assets/js/logic.js
@@ -13,13 +13,13 @@ const feedbackEl = document.querySelector('#feedback');
 
 let score = 0;
 let currentQuestionIndex = 0;
-let timeLeft = 75;
+let timeLeft = 15;
 
 // after the 'Start Quiz' button is clicked, start the timer, hide the #Start-screen div and display the 1st question
 startBtn.addEventListener('click', function() {
     countdown();
     showQuestions();
-    askQuestions();
+    displayQuestion();
 })
 
 // timer function
@@ -27,6 +27,7 @@ function countdown() {
     const timeInterval = setInterval(function() {
     if (timeLeft >= 0) {
         timeEl.textContent = timeLeft;
+        console.log(`Time left is ${timeLeft}`);
         timeLeft--;
     } else {
         timeEl.textContent = 0;
@@ -41,69 +42,63 @@ function showQuestions() {
     questionsDiv.classList.toggle("hide");
 }
 
-// function to cycle through questions
-function askQuestions() {
-    displayQuestion();
+// function to ask questions
+function displayQuestion() {
+    // display question
+    questionTitleEl.textContent = questions[currentQuestionIndex].question;
+    
+    // create div for list of answers
+    const answersList = document.createElement('ol');
+    choicesDiv.appendChild(answersList);
 
-    function displayQuestion() {
-        // display question
-        questionTitleEl.textContent = questions[currentQuestionIndex].question;
-        
-        // create div for list of answers
-        const answersList = document.createElement('ol');
-        choicesDiv.appendChild(answersList);
+    // create answers array
+    const answers = questions[currentQuestionIndex].answers;
 
-        // create answers array
-        const answers = questions[currentQuestionIndex].answers;
-
-        // create lis with answer options, add to the list
-        answers.forEach(answer => {
-            const optionEl = document.createElement('button'); // create html element in DOM for answer option
-            optionEl.classList.add("choices"); // add styling
-            let answerText = document.createTextNode(answer.text); // add a node with answer's text
-            let checkAnswer = answer.correct; // extract info about correctness of the answer
-            optionEl.appendChild(answerText); // add text to the html element
-            answersList.appendChild(optionEl); // add answer option to the list of answers
-            // add event handler for click on answer
-            optionEl.addEventListener('click', (event) => {
-                event.stopPropagation(); // prevent event bubbling
-                // remove previous list of answers for previous questions
-                // answersList.parentNode.removeChild(answersList);
-                choicesDiv.removeChild(answersList);
-                // show feedback for 1 sec
-                if(checkAnswer) {
-                    feedbackEl.textContent = "Correct!";
-                    score += 5;
-                } else {
-                    feedbackEl.textContent = "Incorrect!";
-                    timeLeft -= 5;
-                }
+    // create lis with answer options, add to the list
+    answers.forEach(answer => {
+        const optionEl = document.createElement('button'); // create html element in DOM for answer option
+        optionEl.classList.add("choices"); // add styling
+        let answerText = document.createTextNode(answer.text); // add a node with answer's text
+        let checkAnswer = answer.correct; // extract info about correctness of the answer
+        optionEl.appendChild(answerText); // add text to the html element
+        answersList.appendChild(optionEl); // add answer option to the list of answers
+        // add event handler for click on answer
+        optionEl.addEventListener('click', (event) => {
+            event.stopPropagation(); // prevent event bubbling
+            // remove previous list of answers for previous questions
+            // answersList.parentNode.removeChild(answersList);
+            choicesDiv.removeChild(answersList);
+            // show feedback for 1 sec
+            if(checkAnswer) {
+                feedbackEl.textContent = "Correct!";
+                score += 5;
+            } else {
+                feedbackEl.textContent = "Incorrect!";
+                timeLeft -= 5;
+            }
+            feedbackEl.classList.toggle("hide");
+            setTimeout(function() {
                 feedbackEl.classList.toggle("hide");
-                setTimeout(function() {
-                    feedbackEl.classList.toggle("hide");
-                }, 1000);
-                currentQuestionIndex++; // prepare to move to the next question
-                // display the next question
-                displayQuestion();                
-            });
+            }, 1000);
+            currentQuestionIndex++; // prepare to move to the next question
+            // display the next question
+            displayQuestion();                
         });
-
-        console.log(`Time left is ${timeLeft}`);
-        
-        // if out of time or last question, show End Screen div
-        if (Math.ceil(timeLeft) <= 0 || currentQuestionIndex === questions.length - 1) {
-            questionsDiv.classList.toggle("hide");
-            endScreenDiv.classList.toggle("hide");
-            finalScoreEl.textContent = score;
-            // add event handler on the Submit button
-            submitBtn.addEventListener('click', function(e){
-                e.stopPropagation();
-                const playerInitials = initialsEl.value;
-                const player = {initials: playerInitials, scoreValue: score};
-                const userInfo = JSON.stringify(player);
-                localStorage.setItem("user", userInfo);
-                window.location.href = "highscores.html";
-            });
-        };
+    });
+    
+    if (timeLeft <= 0 || currentQuestionIndex === questions.length - 1) {
+        questionsDiv.classList.toggle("hide");
+        endScreenDiv.classList.toggle("hide");
+        finalScoreEl.textContent = score;
+        console.log(`final time is ${timeLeft}`);
+        // add event handler on the Submit button
+        submitBtn.addEventListener('click', function(e){
+            e.stopPropagation();
+            const playerInitials = initialsEl.value;
+            const player = {initials: playerInitials, scoreValue: score};
+            const userInfo = JSON.stringify(player);
+            localStorage.setItem("user", userInfo);
+            window.location.href = "highscores.html";
+        });
     };
 };
