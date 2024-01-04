994a66c052467ce48dda2d57b9ff60b710f570b3
Inessa <143314129+icollier77@users.noreply.github.com>
2024-01-04 08:07:46 +0000

    removed quotation marks around booleans in questions.js as unnecessary

--- starter/js/questions.js
+++ starter/js/questions.js
@@ -3,200 +3,200 @@ const questions = [
     {
         question: "When is St. David's day?",
         answers: [
-            {text: "1st of March", correct: "true"},
-            {text: "17th of March", correct: "false"},
-            {text: "23rd of April", correct: "false"},
-            {text: "30th of November", correct: "false"}
+            {text: "1st of March", correct: true},
+            {text: "17th of March", correct: false},
+            {text: "23rd of April", correct: false},
+            {text: "30th of November", correct: false}
         ]
     },
     {
         question: "Which British film actor has recently won an Oscar?",
         answers: [
-            {text: "Sean Connery", correct: "false"},
-            {text: "Benedict Cumberbatch", correct: "false"},
-            {text: "Tilda Swinton", correct: "false"},
-            {text: "Anthony Hopkins", correct: "true"}
+            {text: "Sean Connery", correct: false},
+            {text: "Benedict Cumberbatch", correct: false},
+            {text: "Tilda Swinton", correct: false},
+            {text: "Anthony Hopkins", correct: true}
         ]
     },
     {
         question: "Roast beef is a traditional food of which country?",
         answers: [
-            {text: "Northern Ireland", correct: "false"},
-            {text: "England", correct: "true"},
-            {text: "Scotland", correct: "false"},
-            {text: "Wales", correct: "false"}
+            {text: "Northern Ireland", correct: false},
+            {text: "England", correct: true},
+            {text: "Scotland", correct: false},
+            {text: "Wales", correct: false}
         ]
     },
     {
         question: "Which of these is a famous classical music event in the UK?",
         answers: [
-            {text: "T in the Park", correct: "false"},
-            {text: "The Proms", correct: "true"},
-            {text: "Glastonbury Festival", correct: "false"},
-            {text: "Creamfields", correct: "false"}
+            {text: "T in the Park", correct: false},
+            {text: "The Proms", correct: true},
+            {text: "Glastonbury Festival", correct: false},
+            {text: "Creamfields", correct: false}
         ]
     },
     {
         question: "Where is the Cenotaph located?",
         answers: [
-            {text: "Wiltshire", correct: "false"},
-            {text: "Dorset", correct: "false"},
-            {text: "Trafalgar Square", correct: "false"},
-            {text: "Whitehall", correct: "true"}
+            {text: "Wiltshire", correct: false},
+            {text: "Dorset", correct: false},
+            {text: "Trafalgar Square", correct: false},
+            {text: "Whitehall", correct: true}
         ]
     },
     {
         question: "Where was Robert Burns from?",
         answers: [
-            {text: "Northern Ireland", correct: "false"},
-            {text: "England", correct: "false"},
-            {text: "Wales", correct: "false"},
-            {text: "Scotland", correct: "true"},
+            {text: "Northern Ireland", correct: false},
+            {text: "England", correct: false},
+            {text: "Wales", correct: false},
+            {text: "Scotland", correct: true},
         ]
     },
     {
         question: "What is the capital of Wales?",
         answers: [
-            {text: "Cardiff", correct: "true"},
-            {text: "London", correct: "false"},
-            {text: "Belfast", correct: "false"},
-            {text: "Edinburgh", correct: "false"},
+            {text: "Cardiff", correct: true},
+            {text: "London", correct: false},
+            {text: "Belfast", correct: false},
+            {text: "Edinburgh", correct: false},
         ]
     },
     {
         question: "What is not a fundamental principle of British life?",
         answers: [
-            {text: "Driving a car", correct: "true"},
-            {text: "Looking after the environment", correct: "false"},
-            {text: "Treating others with fairness", correct: "false"},
-            {text: "Looking after yourself and your family", correct: "false"}
+            {text: "Driving a car", correct: true},
+            {text: "Looking after the environment", correct: false},
+            {text: "Treating others with fairness", correct: false},
+            {text: "Looking after yourself and your family", correct: false}
         ]
     },
     {
         question: "Who built the Tower of London?",
         answers: [
-            {text: "Oliver Cromwell", correct: "false"},
-            {text: "William the Conquerer", correct: "true"},
-            {text: "Henry VII", correct: "false"},
-            {text: "Henry VIII", correct: "false"}
+            {text: "Oliver Cromwell", correct: false},
+            {text: "William the Conquerer", correct: true},
+            {text: "Henry VII", correct: false},
+            {text: "Henry VIII", correct: false}
         ]
     },
     {
         question: "What flower is associated with England?",
         answers: [
-            {text: "Daffodil", correct: "false"},
-            {text: "Shamrock", correct: "false"},
-            {text: "Thistle", correct: "false"},
-            {text: "Rose", correct: "true"}
+            {text: "Daffodil", correct: false},
+            {text: "Shamrock", correct: false},
+            {text: "Thistle", correct: false},
+            {text: "Rose", correct: true}
         ]
     },
     {
         question: "Who appoints \"Life peers\"?",
         answers: [
-            {text: "The Shadow Cabinet", correct: "false"},
-            {text: "The Monarch", correct: "true"},
-            {text: "The Prime Minister", correct: "false"},
-            {text: "The Speaker", correct: "false"}
+            {text: "The Shadow Cabinet", correct: false},
+            {text: "The Monarch", correct: true},
+            {text: "The Prime Minister", correct: false},
+            {text: "The Speaker", correct: false}
         ]
     },
     {
         question: "Which charity works to preserve important buildings?",
         answers: [
-            {text: "NSPCC", correct: "false"},
-            {text: "Age UK", correct: "false"},
-            {text: "The Red Cross", correct: "false"},
-            {text: "The National Trust", correct: "true"}
+            {text: "NSPCC", correct: false},
+            {text: "Age UK", correct: false},
+            {text: "The Red Cross", correct: false},
+            {text: "The National Trust", correct: true}
         ]
     },
     {
         question: "What Palace was a cast-iron and plate-glass building originally erected in Hyde Park, London, England, to house the Great Exhibition of 1851?",
         answers: [
-            {text: "The Great Palace", correct: "false"},
-            {text: "Gold Palace", correct: "false"},
-            {text: "Dream Palace", correct: "false"},
-            {text: "Crystal Palace", correct: "true"}
+            {text: "The Great Palace", correct: false},
+            {text: "Gold Palace", correct: false},
+            {text: "Dream Palace", correct: false},
+            {text: "Crystal Palace", correct: true}
         ]
     },
     {
         question: "Which of the following is part of the UK?",
         answers: [
-            {text: "St Helena", correct: "false"},
-            {text: "Channel Islands", correct: "false"},
-            {text: "Wales", correct: "true"},
-            {text: "Canada", correct: "false"}
+            {text: "St Helena", correct: false},
+            {text: "Channel Islands", correct: false},
+            {text: "Wales", correct: true},
+            {text: "Canada", correct: false}
         ]
     },
     {
         question: "Which is a British Overseas Territory?",
         answers: [
-            {text: "The Falkland Islands", correct: "true"},
-            {text: "Greenland", correct: "false"},
-            {text: "Ireland", correct: "false"},
-            {text: "Hawaii", correct: "false"},
+            {text: "The Falkland Islands", correct: true},
+            {text: "Greenland", correct: false},
+            {text: "Ireland", correct: false},
+            {text: "Hawaii", correct: false},
         ]
     },
     {
         question: "Who was the first Briton to win the Olympic gold medal in the 10,000 meters?",
         answers: [
-            {text: "Mo Farah", correct: "true"},
-            {text: "David Weir", correct: "false"},
-            {text: "Bradley Wiggins", correct: "false"},
-            {text: "Sir Chris Hoy", correct: "false"}
+            {text: "Mo Farah", correct: true},
+            {text: "David Weir", correct: false},
+            {text: "Bradley Wiggins", correct: false},
+            {text: "Sir Chris Hoy", correct: false}
 
         ]
     },
     {
         question: "When is Christmas Eve?",
         answers: [
-            {text: "24th of December", correct: "true"},
-            {text: "23rd of December", correct: "false"},
-            {text: "25th of December", correct: "false"},
-            {text: "26th of December", correct: "false"}
+            {text: "24th of December", correct: true},
+            {text: "23rd of December", correct: false},
+            {text: "25th of December", correct: false},
+            {text: "26th of December", correct: false}
         ]
     },
     {
         question: "Which of these UK landmarks is in Wales?",
         answers: [
-            {text: "The Giant's Causeway", correct: "false"},
-            {text: "Loch Lomond", correct: "false"},
-            {text: "The Lake District", correct: "false"},
-            {text: "Snowdonia", correct: "true"}
+            {text: "The Giant's Causeway", correct: false},
+            {text: "Loch Lomond", correct: false},
+            {text: "The Lake District", correct: false},
+            {text: "Snowdonia", correct: true}
         ]
     },
     {
         question: "What did the Chartists campaign for?",
         answers: [
-            {text: "The right to vote for women", correct: "false"},
-            {text: "The right to vote for the 21 year olds", correct: "false"},
-            {text: "The right to vote for the 18 year olds", correct: "false"},
-            {text: "The right to vote for the working class", correct: "true"}
+            {text: "The right to vote for women", correct: false},
+            {text: "The right to vote for the 21 year olds", correct: false},
+            {text: "The right to vote for the 18 year olds", correct: false},
+            {text: "The right to vote for the working class", correct: true}
         ]
     },
     {
         question: "When did the first Christian communities appear in Britain?",
         answers: [
-            {text: "4th and 5th centuries", correct: "false"},
-            {text: "3rd and 4th centuries", correct: "true"},
-            {text: "2nd and 3rd centuries", correct: "false"},
-            {text: "1st and 2nd centuries", correct: "false"}
+            {text: "4th and 5th centuries", correct: false},
+            {text: "3rd and 4th centuries", correct: true},
+            {text: "2nd and 3rd centuries", correct: false},
+            {text: "1st and 2nd centuries", correct: false}
         ]
     },
     {
         question: "Which flag has a white cross on a blue background?",
         answers: [
-            {text: "Irish", correct: "false"},
-            {text: "Scottish", correct: "true"},
-            {text: "English", correct: "false"},
-            {text: "Welsh", correct: "false"}
+            {text: "Irish", correct: false},
+            {text: "Scottish", correct: true},
+            {text: "English", correct: false},
+            {text: "Welsh", correct: false}
         ]
     },
     {
         question: "Who was Queen Elizabeth II married to?",
         answers: [
-            {text: "King Charles III", correct: "false"},
-            {text: "Prince William", correct: "false"},
-            {text: "Prince Harry", correct: "false"},
-            {text: "Prince Philip", correct: "true"}
+            {text: "King Charles III", correct: false},
+            {text: "Prince William", correct: false},
+            {text: "Prince Harry", correct: false},
+            {text: "Prince Philip", correct: true}
         ]
     }
 ]
